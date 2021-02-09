bb7925f7f2767307e5c102fb0bc88294bbf8866d
jiangjkd <jiangkundi@warmrabbit.onaliyun.com>
2021-02-09 16:35:39 +0800

    fix

---
+++ scripts/init.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "*** Initializing WASM build environment ***"
+
+if [ -z $CI_PROJECT_NAME ] ; then
+    rustup update nightly
+    rustup update stable
+fi
+
+rustup target add wasm32-unknown-unknown --toolchain nightly
+
+#cargo +stable build --release
